f1aacaf2f955a8fde6f117399f1d7192d53ece6a
sdulipalla <shasikanth.dulipalla@realpage.com>
2022-02-06 00:16:37 +0530

    Hello world - Create-React-App

--- src/App.js
+++ src/App.js
@@ -4,19 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3>Hello World</h3>
+      <h4>Welcome to React!</h4>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am from App functional component... please edit <code>src/App.js</code> and save to reload.</p>
       </header>
     </div>
   );
